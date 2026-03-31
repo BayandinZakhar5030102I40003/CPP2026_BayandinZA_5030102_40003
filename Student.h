5ae98ed8b23433cf22c2927f19447dfce6f060f5
BayandinZakhar5030102I40003 <bayandin.za@edu.spbstu.ru>
2026-03-31 23:01:39 +0300

    .

--- Student.h
+++ Student.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <string>
+//#include ""
+//
+//class Student {
+// public:
+//  Student(std::string name);
+//  virtual Soluthion 
+//  ~Student();
+//
+// private:
+//  std::string name;
+//
+//};
+//
+//Student::Student() {}
+//
+//Student::~Student() {}
