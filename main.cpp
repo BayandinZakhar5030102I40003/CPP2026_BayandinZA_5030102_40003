787550b6a3a4e4c10e18cebc4a36a7fe794e6222
BayandinZakhar5030102I40003 <bayandin.za@edu.spbstu.ru>
2026-04-03 23:16:20 +0300

    Add main.cpp_01

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
+#include <fstream>
 #include <iostream>
 #include <map>
 #include <string>
@@ -23,6 +24,12 @@ class Equation {
   Equation(double a, double b, double c) : a(a), b(b), c(c) {}
   Solution solve() const {
     double D = b * b - 4 * a * c;
+
+    if (a == 0) {
+      if (b == 0) return Solution(0);
+      double x = -c / b;
+      return Solution(1, x, x);
+    }
 
     if (D > 0) {
       double x1 = (-b + sqrt(D)) / (2 * a);
@@ -65,7 +72,7 @@ class Mid_S : public Student {
     if (chance < 0.5) {
       return ans.solve();
     } else {
-      return Solution(1, std::rand() % 10);
+      return Solution(1, std::rand() % 10, std::rand() % 10);
     }
   }
 };
@@ -74,7 +81,7 @@ class Bad_S : public Student {
  public:
   Bad_S(std::string name) : Student(name) {}
 
-  Solution solve(const Equation& ans) override { return Solution(1, 10); }
+  Solution solve(const Equation& ans) override { return Solution(1, 10, 10); }
 };
 
 class Letter {
@@ -85,7 +92,92 @@ class Letter {
 
   Letter(std::string name, Equation fnc, Solution ans)
       : studName(name), func(fnc), answer(ans) {}
-
 };
 
+class Teacher {
+ private:
+  std::vector<Letter> queue;
+  std::map<std::string, int> results;
 
+  bool isCorrect(const Solution& correct, const Solution& student) {
+    const double EPS = 1e-6;
+
+    if (correct.rootsCount != student.rootsCount) return false;
+
+    if (correct.rootsCount == 1)
+      return std::fabs(correct.x1 - student.x1) < EPS;
+
+    if (correct.rootsCount == 2)
+      return (std::fabs(correct.x1 - student.x1) < EPS &&
+              std::fabs(correct.x2 - student.x2) < EPS) ||
+             (std::fabs(correct.x1 - student.x2) < EPS &&
+              std::fabs(correct.x2 - student.x1) < EPS);
+
+    return true;
+  }
+
+ public:
+  void addSubmission(const Letter& sub) { queue.push_back(sub); }
+
+  void checkAll() {
+    for (const auto& sub : queue) {
+      results[sub.studName] = 0;
+    }
+
+    for (const auto& sub : queue) {
+      Solution correct = sub.func.solve();
+
+      if (isCorrect(correct, sub.answer)) {
+        results[sub.studName]++;
+      }
+    }
+  }
+
+  void printResults() {
+    for (const auto& p : results) {
+      std::cout << p.first << " - " << p.second << std::endl;
+    }
+  }
+};
+
+int main() {
+  std::srand(std::time(0));
+
+  std::vector<Equation> equations;
+
+  std::ifstream file("equations.txt");
+  if (!file.is_open()) {
+    std::cerr << "Erorr" << std::endl;
+    return 1;
+  }
+
+  double a, b, c;
+  while (file >> a >> b >> c) {
+    equations.emplace_back(a, b, c);
+  }
+  file.close();
+
+  std::vector<Student*> students = {
+      new Mid_S("Sergey"), new Good_S("Anton"), new Mid_S("Petr"),
+      new Bad_S("Sasha"),  new Mid_S("Timur"),  new Mid_S("Timofei"),
+      new Bad_S("Zakhar"), new Mid_S("Roman"),  new Good_S("Rafael Ambrosius Kusto"),
+  };
+
+  Teacher teacher;
+
+  for (auto student : students) {
+    for (const auto& eq : equations) {
+      Solution ans = student->solve(eq);
+      teacher.addSubmission(Letter(student->getName(), eq, ans));
+    }
+  }
+
+  teacher.checkAll();
+  teacher.printResults();
+
+  for (auto s : students) {
+    delete s;
+  }
+
+  return 0;
+}
